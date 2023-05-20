8ba1d66656c1a47cdd9e4d03e2bdd69ad7b03185
Bea-ro <b_a_ro@hotmail.com>
2023-05-20 13:34:48 +0200

    fix images path

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './GameCard.css';
 
+
 const GameCard = ({ game, image, description }) => {
   return (
     <div className="game-card">
       <h1 className="game-name">{game}</h1>
-      <img src={`../../../public/${image}`} alt={game} className="game-image" />
+      <img src={`../../${image}`}
+      alt={game} className="game-image" />
       <p className="game-description" dangerouslySetInnerHTML={{ __html: description }}></p>
     </div>
   );
